a8c8c0a9671e0f358cef38585d86e32ab421922a
Henry Lee <contact@henrylee.io>
2019-01-24 09:46:45 +1100

    update app component into functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,24 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import Location from 'components/Location';
 import RacingList from 'components/RacingList';
 import RacingTypeFilter from 'components/RacingTypeFilter';
-class App extends PureComponent {
-  render() {
-    return (
-      <div className="App">
-        <nav className="nav">
-          <div className="content">
-            <Location />
-          </div>
-        </nav>
-        <div className="main content ">
-          <h1>Next To Go</h1>
-          <div>
-            <RacingTypeFilter />
-          </div>
-          <RacingList />
-        </div>
-      </div>
-    );
-  }
-}
 
+const App =  props =>{
+  return (
+    <div className="App">
+      <nav className="nav">
+        <div className="content">
+          <Location />
+        </div>
+      </nav>
+      <div className="main content ">
+        <h1>Next To Go</h1>
+        <div>
+          <RacingTypeFilter />
+        </div>
+        <RacingList />
+      </div>
+    </div>
+  );
+}
 export default App;
